60eb9b6b3de98eee8cfa29173c95a6cbb2f7f500
Igor Anić <igor.anic@gmail.com>
2024-03-19 21:55:46 +0100

    remove unused lines

--- run.sh
+++ run.sh
@@ -1,14 +1,9 @@
 #! /bin/bash
 
-
 function run {
 	# fill cache
 	rm -rf $cache
 	$zig_bin build --global-cache-dir $cache
-
-	# error BadFileName stripped to empty file name
-	#$zig_bin fetch https://chromium.googlesource.com/chromium/tools/depot_tools.git/+archive/7b8f3503d64cf530cff5f4e9e88e3e80e7529b95.tar.gz
-	#$zig_bin fetch https://zig-gamedev-pkgs.ams3.cdn.digitaloceanspaces.com/zmath-0.9.6.tar.gz
 }
 
 echo master build
@@ -21,4 +16,5 @@ zig_bin=zig
 cache=./zig-global-cache-release
 run
 
+echo compare master and release
 diff --brief --recursive --no-dereference zig-global-cache-release/p zig-global-cache-master/p
